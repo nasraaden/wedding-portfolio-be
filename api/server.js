f5b9addafb0a7b82536234dd5b7966951f470b31
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-03-14 18:10:10 -0700

    add models/routers and set up server.js

--- api/server.js
+++ api/server.js
@@ -10,12 +10,21 @@ server.use(cors());
 server.use(morgan('tiny'));
 
 // auth middleware
-const { restricted } = require('../middleware/index.js');
+const { restricted } = require('../middleware/restricted.js');
+
+// routers
+const authRouter = require('../routers/auth-router.js');
+const usersRouter = require('../routers/users-router.js');
+const postsRouter = require('../routers/posts-router.js');
 
 server.get('/', (req, res) => {
   res.status(200).json({
     api: `is up and running!`
   });
 });
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', restricted, usersRouter);
+server.use('/api/posts', postsRouter);
 
 module.exports = server;
